52a0775a52698a4247b7e09566643cfe3126ad9f
soeren.poppe <soeren.poppe@gmx.de>
2022-05-22 14:11:01 +0200

    add pixel font, add collected animation, add bubble sound, add hud

---
+++ pickups/Coin.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+const score_value = 1
+var collected = false
+
+func _on_Coin_body_entered(body: Node) -> void:
+	$AnimatedSprite.play("collected")
+	if !collected:
+		$AudioStreamPlayer2D.play()
+		Events.emit_signal("score_changed", score_value)
+		collected = true
+
+
+
+func _on_AudioStreamPlayer2D_finished() -> void:
+	print("played sound")
+	queue_free()
